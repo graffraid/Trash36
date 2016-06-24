37335b619b6329c08224b3a969befe760b0aa9ee
Roman Ruzin <graffraid@mail.ru>
2016-06-24 17:52:35 +0300

    Add NewVehicleModel

---
+++ Trash.MVC/Models/NewVehicleModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trash.MVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class NewVehicleModel
+    {
+        public int? VehicleBrandId { get; set; }
+        public int? VehicleModelId { get; set; }
+        public int? VehicleBodyId { get; set; }
+        public int? Year { get; set; }
+        public string Vin { get; set; }
+        public int? Mileage { get; set; }
+        public bool? LeftHandDrive { get; set; }
+        public int DriveTypeId { get; set; }
+        public int TransmissionTypeId { get; set; }
+        public string Engine { get; set; }
+        public string Description { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public double? PurchasePrice { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
